6fb50c75392a99aa30169540f4edd31fe0833c69
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-04 19:38:22 +0530

    Adding mobile menu button

--- components/Navbar-Mobile.tsx
+++ components/Navbar-Mobile.tsx
@@ -1,17 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
 const NavbarMobile = () => {
-    return (
-        <main>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </main>
-    )
-}
+  const [click, setClick] = useState(false);
+  return (
+    <nav className="flex items-center justify-between">
+      <div>
+        <Link
+          href="/"
+          className="flex gap-3 items-center hover:scale-105 transition-all ease-in-out duration-300"
+        >
+          <Image src="/check-list.png" alt="logo" width={40} height={40} />
+          <h1 className="font-black text-xl">Todo List</h1>
+        </Link>
+      </div>
+      <div>
+        <ul className="flex flex-col gap-1" onClick={() => setClick(!click)}>
+          <li className={`bg-black w-8 h-1 transition-all ease-in-out duration-300 ${click && "-rotate-45 origin-top-right"}`}></li>
+          <li className={`bg-black w-8 h-1 transition-all ease-in-out duration-300 ${click && "bg-white"}`}></li>
+          <li className={`bg-black w-8 h-1 transition-all ease-in-out duration-300 ${click && "rotate-[48deg] origin-[25px_6px]"}`}></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default NavbarMobile;
